064a9e8e5de467c5953b8d279306cd7b8d9f2ee6
Alex DeCapri <alexdecapri@gmail.com>
2016-08-09 13:35:03 -0600

    createPost action creator working in redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props; // same as const handleSubmit = this.props.handleSubmit;
-    console.log(title);
+    // console.log(title);
 
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
         <div className="form-group">
           <label>Title</label>
@@ -28,7 +29,10 @@ class PostsNew extends Component {
   }
 }
 
+// connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStatetoProps (null here), 3rd is mapDispatchToProps (action creator createPost here)
+
 export default reduxForm({
   form: 'PostsNewForm', // just needs to be unique
   fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
