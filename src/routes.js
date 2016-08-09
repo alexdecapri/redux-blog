0797ed2d650f5afb1b8221837f5e1ef40620fb61
Alex DeCapri <alexdecapri@gmail.com>
2016-08-09 16:15:38 -0600

    model URL for posts_show

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 export default (
   <Route path="/" component={App}>
@@ -12,3 +13,5 @@ export default (
     <Route path="posts/:id" component={PostsShow} />
   </Route>
 );
+
+// this.props.params.id
