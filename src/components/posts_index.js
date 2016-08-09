4816f1d9676a7b8c8d63428977405d688bacdf40
Alex DeCapri <alexdecapri@gmail.com>
2016-08-09 16:09:22 -0600

    posts render to index

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -8,8 +8,19 @@ class PostsIndex extends Component {
   componentWillMount() {
       // console.log("this would be a good time to call an action creator to fetch posts");
       this.props.fetchPosts(); // made possible by mapDispatchToProps, bindActionCreators, and finally connect
+      // now need to mapStateToProps to be able to render
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+        </li>
+      )
+    })
+  }
 
   render() {
     return (
@@ -19,16 +30,23 @@ class PostsIndex extends Component {
             Add a Post
           </Link>
         </div>
-        List of blog posts
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts.all }
 }
 
 // function mapDispatchToProps(dispatch) {
 //   return bindActionCreators({ fetchPosts }, dispatch)
 // }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
 // could have been  { fetchPosts: fetchPosts }
 // go rid of mapDispatchToProps
