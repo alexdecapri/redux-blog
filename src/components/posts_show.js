a0dd1f7d85789d2a8ef9a1e9e454013dd4a32a01
Alex DeCapri <alexdecapri@gmail.com>
2016-08-09 16:30:35 -0600

    individual posts now viewable

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -8,10 +8,24 @@ class PostsShow extends Component {
   }
 
   render() {
+    const { post } = this.props;
+    // console.log(this.props.post);
+    if (!post) {
+      return <div>Loading...</div>
+    }
+
     return (
-      <div>Show post {this.props.params.id}</div>
+      <div>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
     );
   }
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+function mapStateToProps(state) {
+  return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
