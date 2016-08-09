751f932d8e90d609c1a83ff6f45ae449d72d27f5
Alex DeCapri <alexdecapri@gmail.com>
2016-08-09 16:13:40 -0600

    new route for individual posts

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,10 @@ import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
 
-
-
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex} /> // If route ONLY matches parent, just show "/" route and not children
     <Route path="posts/new" component={PostsNew} />
+    <Route path="posts/:id" component={PostsShow} />
   </Route>
 );
