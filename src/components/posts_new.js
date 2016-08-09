a47df7b38f609e62f22910233c225f0a22fe6ef9
Alex DeCapri <alexdecapri@gmail.com>
2016-08-09 12:44:52 -0600

    redux-form now manages form elements

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,20 +3,23 @@ import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props; // same as const handleSubmit = this.props.handleSubmit;
+    console.log(title);
+
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Create A New Post</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title} />
         </div>
         <div className="form-group">
-          <label>Cateogories</label>
-          <input type="text" className="form-control" />
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories} />
         </div>
         <div className="form-group">
           <label>Content</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content} />
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
