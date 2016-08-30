66b89fddc294439ba0f1807bf4cfe66133e350c4
Alex DeCapri <alexdecapri@gmail.com>
2016-08-30 15:10:05 -0600

    heroku deploy

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var port  = process.env.PORT ? JSON.parse(process.env.PORT) : 8080;
 console.log('-- port:  ' + port);
 
 var app  = express();
-var root = __dirname + '/build';
+var root = __dirname + '/';
 
 // Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
 // a load balancer (e.g. Heroku). See further comments below
