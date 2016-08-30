9e6c35f29b23ca2ad4076c13e141e460ca93bdf2
Alex DeCapri <alexdecapri@gmail.com>
2016-08-30 15:19:05 -0600

    updateS

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ module.exports = {
     './src/index.js'
   ],
   output: {
-    path: __dirname,
-    publicPath: '/build/',
+    path: './build',
+    publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
